ab968bb7e540fb3dd60d610655e3e47200f47b68
Dielli Demjaha <diellidemjaha@gmail.com>
2025-02-27 15:19:37 +0100

    objects dynamic constructors added

---
+++ object-constructors-dynamic/script-2.js
@@ -0,0 +1,14 @@
+function Person(name, last, age, eyes) {
+    this.firstName = name
+    this.lastName = last
+    this.age = age
+    this.eyeColor = eyes
+}
+
+const Dielli = new Person("Dielli", "Demjaha", "35", "kafte");
+
+let results = document.querySelector(".results");
+
+results.textContent = `Emri dhe Mbiemri: ${Dielli.firstName} ${Dielli.lastName}, dhe eshte ${Dielli.age} vjecar.`;
+
+let button = document.querySelector(".button");
